dfd1ac26f9b2db0948e2d9690091344496889f77
Kseniya <xenista@mail.ru>
2024-05-27 23:07:47 +0500

    add catalog page and json-server

---
+++ src/components/products/ProductCard.vue
@@ -0,0 +1,92 @@
+<script setup>
+    defineProps({
+        product: Object,
+        loading: Boolean
+    })
+</script>
+
+<template>
+    <v-card
+    :disabled="loading"
+    :loading="loading"
+    class="mx-auto product-card"
+    max-width="374"
+  >
+    <template v-slot:loader="{ isActive }">
+      <v-progress-linear
+        :active="isActive"
+        color="deep-purple"
+        height="4"
+        indeterminate
+      ></v-progress-linear>
+    </template>
+
+    <v-img
+      height="250"
+      :src="product.image"
+      
+    ></v-img>
+
+    <v-card-item>
+      <v-card-title>{{ product.title }}</v-card-title>
+
+      <v-card-subtitle>
+        <span class="me-1">{{ product.category}}</span>
+
+        <v-icon
+          color="error"
+          icon="mdi-fire-circle"
+          size="small"
+        ></v-icon>
+      </v-card-subtitle>
+    </v-card-item>
+
+    <v-card-text>
+      <v-row
+        align="center"
+        class="mx-0"
+      >
+        <v-rating
+          :model-value="product.rating.rate"
+          color="amber"
+          density="compact"
+          size="small"
+          half-increments
+          readonly
+        ></v-rating>
+
+        <div class="text-grey ms-4">
+          {{product.rating.rate}} ({{product.rating.count}})
+        </div>
+      </v-row>
+
+      <div class="my-4 text-subtitle-1">₽ {{product.price}}
+      </div>
+
+      <p class="product-description">{{product.description}}</p>
+    </v-card-text>
+
+
+    <v-card-actions class="mt-auto">
+      <v-btn
+        color="deep-purple-lighten-2"
+        text="В корзину"
+        block
+        border
+      ></v-btn>
+    </v-card-actions>
+    </v-card>
+</template>
+
+<style scoped>
+.product-description{
+  display: -webkit-box;
+-webkit-line-clamp: 3;
+-webkit-box-orient: vertical;
+overflow: hidden;
+text-overflow: ellipsis;
+}
+.product-card{
+  height: 100%;
+}
+</style>
---
+++ src/pages/CatalogPage.vue
@@ -0,0 +1,43 @@
+<script setup>
+import ProductCard from '../components/products/ProductCard.vue'
+import { ref } from "vue"
+const loading = ref(true)
+const products = ref([])
+
+async function fetchProducts() {
+  try {
+    loading.value = true;
+    const response = await fetch('http://localhost:3000/products');
+    products.value = await response.json();
+    loading.value = false
+  } catch (error) {
+    console.error(error);
+    loading.value = false
+  }
+}
+setTimeout(() => {
+  fetchProducts()
+}, 2000)
+
+
+</script>
+
+<template>
+   <v-container>
+    <h1 class="mb-12">Каталог товаров</h1>
+    <v-row v-if="loading">
+      <v-col cols="4" v-for="index in 7" :key="index">
+        <v-skeleton-loader type="image, heading, paragraph, button">
+        </v-skeleton-loader>
+      </v-col>
+    </v-row >
+    <v-row v-else>
+      <v-col cols="4" v-for="product in products" :key="product.id">
+        <product-card :product="product" :loading="loading"/>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+</style>
